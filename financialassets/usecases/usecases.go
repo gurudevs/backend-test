d497699f24968a39fc3306b52d40f01464284465
Ferkze <ferkemartins@gmail.com>
2020-08-08 17:01:39 -0300

    Adição de usecases e implementação

---
+++ financialassets/usecases/usecases.go
@@ -0,0 +1,33 @@
+package usecases
+
+import (
+	"github.com/ferkze/backend-test/financialassets/domain/model"
+)
+
+// FinancialAssetsUsecases casos de uso do serviço
+type FinancialAssetsUsecases interface {
+	GetAssetsOrderedByVariation() ([]*model.FinancialAsset, error)
+}
+
+// FinancialAsset Estrutura DAO do ativo financeiro
+type FinancialAsset struct {
+
+	Ticker string
+	Company string
+	Close float32
+	Open float32
+	Price float32
+	PctVariation float32
+	PriceVariation float32
+
+}
+
+func toFinancialAsset(assets []*model.FinancialAsset) ([]*FinancialAsset) {
+	res := make([]*FinancialAsset, len(assets))
+	for i, asset := range assets {
+		res[i] = &FinancialAsset{
+			Ticker: asset.GetTicker(),
+		}
+	}
+	return res
+}
---
+++ financialassets/usecases/usecases_impl.go
@@ -0,0 +1,27 @@
+package usecases
+
+import (
+	"github.com/ferkze/backend-test/financialassets/domain/model"
+	"github.com/ferkze/backend-test/financialassets/repositories"
+)
+
+// financialAssetsUsecaseImpl estrutura do contrado de implementação do caso de uso
+type financialAssetsUsecasesImpl struct {
+	repo repositories.FinancialAssetRepository
+}
+
+// NewFinancialAssetsUsecases retorna instancia da implementação do caso de uso
+func NewFinancialAssetsUsecases(repo repositories.FinancialAssetRepository) FinancialAssetsUsecases {
+	return &financialAssetsUsecasesImpl{
+		repo: repo,
+	}
+}
+
+// GetAssetsOrderedByVariation Seleciona todos os ativos ordenados por variação descendente
+func (uc *financialAssetsUsecasesImpl) GetAssetsOrderedByVariation() ([]*model.FinancialAsset, error) {
+	assets, err := uc.repo.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	return assets, nil
+}
