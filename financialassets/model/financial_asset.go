dbcdf5fbd94e4302b75985c67f77b85f89063452
Ferkze <ferkemartins@gmail.com>
2020-08-09 03:18:13 -0300

    Adicionado json name dos ativos

--- financialassets/model/financial_asset.go
+++ financialassets/model/financial_asset.go
@@ -1,15 +1,17 @@
 package model
 
+import "time"
+
 // FinancialAsset Ativo financeiro
 type FinancialAsset struct {
-	Ticker string
-	Company string
-	Close float32
-	Open float32
-	Price float32
-	PctVariation float32
-	PriceVariation float32
-
+	Ticker         string  `json:"ticker"`
+	Company        string  `json:"company"`
+	Close          float32 `json:"close"`
+	Open           float32 `json:"open"`
+	Price          float32 `json:"price"`
+	PctVariation   float32 `json:"pctVariation"`
+	PriceVariation float32 `json:"priceVariation"`
+	updatedAt      time.Time
 }
 
 // NewFinancialAsset instanciar novo ativo financeiro
@@ -22,4 +24,9 @@ func NewFinancialAsset(ticker string) *FinancialAsset {
 // GetTicker retorna o ticker do ativo financeiro
 func (fa *FinancialAsset) GetTicker() string {
 	return fa.Ticker
+}
+
+// GetUpdatedAt retorna o objeto de tempo de sua ultima atualização
+func (fa *FinancialAsset) GetUpdatedAt() time.Time {
+	return fa.updatedAt
 }
